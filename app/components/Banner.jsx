3e0444d0adc6b2d517a4aff38528064b641a3060
chiranjeevi <developer.chiranjeevim@gmail.com>
2025-04-27 21:27:06 +0530

    Created Banner Component and Made it Responsive Across All Screens

---
+++ app/components/Banner.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React from "react";
+import dynamic from "next/dynamic";
+
+
+
+const WaterWave = dynamic(() => import('react-water-wave'), { ssr: false });
+
+const BannerText = () => {
+
+  return(
+    <div className="text-center my-[4rem] mx-[0.5rem]">
+      <h1 className="text-white text-4xl md:text-5xl">Hi, I am <span className="text-[#fec544]">Chiranjeevi</span></h1>
+      <p className="text-[#a9adb8] mt-[1.5rem] text-[1rem] md:text-[1.25rem]">I am a frontend web developer. I can provide clean code and pixel perfect design.</p>
+      <p className="text-[#a9adb8] text-[1rem] md:text-[1.25rem] ">I also make website more & more interactive with web animations.</p>
+    </div>
+  );
+};
+
+
+
+const Banner = () => {
+  return (
+    <div className="">
+      <WaterWave
+        imageUrl={"/assets/images/banner.jpg"}
+        >
+        {methods=> (
+        /* children components */
+        <div className="w-full md:h-[calc(100vh-4.75rem)] flex items-center justify-center">
+          <BannerText />
+        </div>
+        )}
+    </WaterWave>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+
+import React,{ useState } from "react";
+
+
+
+
+const Navbar : React.FC = () => {
+
+    const [isNavbarActive, setNavbarActive] = useState(true);
+
+    return(
+        <>
+            <div className="relative">
+                <div className="fixed top-0 z-20 w-full bg-[#212529] border-b-[1px] border-[#a9adb8] py-2 px-[1rem] md:px-[5rem] flex items-center justify-between">
+                    {/* navbar logo container */}
+                    <div className="select-none">
+                        <h1 className="capitalize text-white text-[2.5rem]"><span className="text-[#fec544]">c</span>j</h1>
+                    </div>
+
+                    {/* navbar menu container */}
+                    <div className="hidden md:block">
+                        <ul className="text-white inline-flex items-center select-none">
+                            <li className="uppercase mr-[1.5rem] hover:text-[#fec544] duration-600"><a href="#" className="">home</a></li>
+                            <li className="uppercase mr-[1.5rem] hover:text-[#fec544] duration-600"><a href="#" className="">about</a></li>
+                            <li className="uppercase mr-[1.5rem] hover:text-[#fec544] duration-600"><a href="#" className="">resume</a></li>
+                            <li className="uppercase mr-[1.5rem] hover:text-[#fec544] duration-600"><a href="#" className="">portfolio</a></li>
+                            <li className="uppercase mr-[1.5rem] hover:text-[#fec544] duration-600"><a href="#" className="">contact</a></li>
+                        </ul>
+                    </div>
+
+                    {/* navbar toggler icon */}
+                    <div className="block md:hidden" onClick={() => setNavbarActive(!isNavbarActive)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 text-[#a9adb8]">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        </svg>
+                    </div>
+                </div>
+                <div className={`absolute z-10 ${isNavbarActive?"duration-600 top-18" : "-top-140 duration-600"} w-full bg-[#212529] h-[20rem] px-[1rem] md:hidden`}>
+                    <ul className="text-[#a9adb8] text-[1.2rem] select-none">
+                        <li className="uppercase mt-[1.5rem] hover:text-[#fec544] duration-600"><a href="#" className="">home</a></li>
+                        <li className="uppercase mt-[1.5rem] hover:text-[#fec544] duration-600"><a href="#" className="">about</a></li>
+                        <li className="uppercase mt-[1.5rem] hover:text-[#fec544] duration-600"><a href="#" className="">resume</a></li>
+                        <li className="uppercase mt-[1.5rem] hover:text-[#fec544] duration-600"><a href="#" className="">portfolio</a></li>
+                        <li className="uppercase mt-[1.5rem] hover:text-[#fec544] duration-600"><a href="#" className="">contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="w-full h-[4.75rem]">
+
+            </div>
+        </>
+    );
+};
+
+
+export default Navbar;
